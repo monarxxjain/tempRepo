0aa08afdaa217d4f0c209fa60a5379cd23f9c468
monarxxjain <jainmonark1000@gmail.com>
2024-01-26 16:46:44 +0530

    Done

---
+++ src/ChatComponent.jsx
@@ -0,0 +1,9 @@
+// ChatComponent.jsx
+import React from 'react';
+
+const ChatComponent = ({ password }) => {
+  // Your chat component logic here
+  return <div>This is the chat component -- {password}</div>;
+};
+
+export default ChatComponent;
